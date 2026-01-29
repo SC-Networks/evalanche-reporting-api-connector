5d14fe9c13f8deeac5d8b56d3881ca37b7791387
Daniel Jakob <jakob@sc-networks.com>
2026-01-29 15:12:06 +0100

    Update dependencies

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,16 +1,19 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-    ->in(['src', 'tests'])
-;
-$config = new PhpCsFixer\Config();
-$config->setRules(
-    [
-        '@PSR12' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline', 'keep_multiple_spaces_after_comma' => false],
-    ]
-)->setFinder($finder)
+declare(strict_types=1);
+
+$config = (new PhpCsFixer\Config())
+    ->setUnsupportedPhpVersionAllowed(true)
+    ->setRules(
+        [
+            '@PSR12' => true,
+            'array_syntax' => ['syntax' => 'short'],
+            'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline', 'keep_multiple_spaces_after_comma' => false],
+        ]
+    )->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in(['src', 'tests'])
+    )
 ;
 
 return $config;
